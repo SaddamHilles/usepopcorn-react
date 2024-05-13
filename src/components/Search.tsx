0a6c43d5e116e7a5a4c9869a42a90604966c6828
SaddamHilles <sdm.hilles@gmail.com>
2024-05-14 02:24:45 +0300

    implement search input feature

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface Props {
-  query: string;
-  setQuery: (movieName: string) => void
+    query: string;
+    setQuery: (movieName: string) => void;
 }
-function Search({query, setQuery}: Props) {
+function Search({ query, setQuery }: Props) {
+    const searchRef = useRef<HTMLInputElement>(null);
 
-  return (
-    <input
-      className='search'
-      type='text'
-      placeholder='Search movies...'
-      value={query}
-      onChange={e => setQuery(e.target.value)}
-    />
-  );
+    useEffect(() => {
+        const handleKeydown = (e: KeyboardEvent) => {
+            if (document.activeElement === searchRef.current) return;
+
+            if (e.code === 'Enter' && searchRef.current) {
+                searchRef.current.focus();
+                setQuery('');
+            }
+        };
+        document.addEventListener('keydown', handleKeydown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeydown);
+        };
+    }, [setQuery]);
+    
+    return (
+        <input
+            ref={searchRef}
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+        />
+    );
 }
 
 export default Search;
--- src/components/WatchedSummary.tsx
+++ src/components/WatchedSummary.tsx
@@ -5,7 +5,6 @@ interface Props {
 }
 
 function WatchedSummary({ watched }: Props) {
-    console.log('watched: ', watched);
     const avgImdbRating = average(
         watched.map(movie => parseInt(movie.imdbRating) || 0)
     );
